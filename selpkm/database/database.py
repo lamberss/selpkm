d3bb211913cd0f28a1d7d155e39a53cbc34fe547
Steven E. Lamberson, Jr <steven.lamberson@gmail.com>
2023-06-02 17:28:33 -0600

    Add some add and get routines for containers

--- selpkm/database/database.py
+++ selpkm/database/database.py
@@ -11,6 +11,7 @@ class Database(object):
         self._path = path
         self._connection = sqlite3.connect(str(self._path))
         self._connection.execute('PRAGMA foreign_keys = 1')
+        self._connection.row_factory = sqlite3.Row
 
         if initialize:
             if self.get_version() is None:
@@ -57,8 +58,56 @@ class Database(object):
             if not all_versions:
                 return []
             else:
-                return [a[0] for a in sorted(all_versions)]
-
+                return [a['version_id'] for a in sorted(all_versions)]
+            
+    def add_container(self, name, parent_id=None, parent_name=None) -> int:
+        timestamp = get_timestamp()
+        if parent_id is not None or parent_name is not None:
+            parent = self.get_container(id=parent_id, name=parent_name)
+        else:
+            parent = {'container_id': None}
+        try:
+            with self._connection as conn:
+                conn.execute('INSERT INTO containers(name,parent_id,created,modified) VALUES(?,?,?,?)',
+                             (name, parent['container_id'], timestamp, timestamp))
+        except sqlite3.IntegrityError as exception:
+            if exception.args[0] == 'UNIQUE constraint failed: containers.name':
+                raise ContainerNameExists(name)
+            elif exception.args[0] == 'FOREIGN KEY constraint failed':
+                raise ContainerDoesNotExist(parent_id)
+            else:
+                raise exception
+        with self._connection as conn:
+            result = conn.execute('SELECT * FROM containers WHERE name=?', (name,))
+        return result.fetchone()['container_id']
+    
+    def get_container(self, id=None, name=None) -> dict:
+        sql = 'SELECT * FROM containers'
+        sql_where = []
+        params = []
+        if id is not None:
+            sql_where.append('container_id=?')
+            params.append(id)
+        if name is not None:
+            sql_where.append('name=?')
+            params.append(name)
+        if sql_where:
+            sql = f'{sql} WHERE {" AND ".join(sql_where)}'
+        with self._connection as conn:
+            result = conn.execute(sql, tuple(params))
+        row = result.fetchone()
+        if row is None:
+            raise ContainerDoesNotExist(id=id, name=name)
+        return dict(row)
+    
+    def get_containers(self) -> List[dict]:
+        with self._connection as conn:
+            result = conn.execute('SELECT * FROM containers')
+        rows = result.fetchall()
+        if rows is None:
+            return []
+        return [dict(r) for r in rows]
+            
     def get_tables(self) -> List[str]:
         return get_tables(self._connection)
 
@@ -68,3 +117,32 @@ class Database(object):
             return None
         else:
             return all_versions[-1]
+
+
+class DatabaseException(Exception):
+    pass
+
+class ContainerDoesNotExist(DatabaseException):
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name
+        labels = []
+        if id is not None:
+            labels.append(f'id={self.id}')
+        if name is not None:
+            labels.append(f'name="{self.name}"')
+        label_str = ''
+        if labels:
+            label_str = f'with ({",".join(labels)}) '
+        message = f'Container {label_str}does not exist.'
+        self.message = message
+        super().__init__(message, id, name)
+
+    def __str__(self):
+        return str(self.message)
+
+class ContainerNameExists(DatabaseException):
+    def __init__(self, name):
+        self.name = name
+        message = f'Cannot add container named "{self.name}", it already exists.'
+        super().__init__(message)
--- selpkm/database/tests/test_database.py
+++ selpkm/database/tests/test_database.py
@@ -1,9 +1,73 @@
+import sqlite3
 import unittest
 
-from ..database import Database
+from ..database import Database, ContainerDoesNotExist, ContainerNameExists
 
 
 class TestDatabase(unittest.TestCase):
+    def test_add_container(self):
+        db = Database(':memory:')
+        self.assertEqual(db.add_container('thing 1'), 1)
+        self.assertEqual(db.add_container('thing 2'), 2)
+        self.assertEqual(db.add_container('thing 3', parent_id=1), 3)
+        self.assertEqual(db.add_container('thing 4', parent_name='thing 1'), 4)
+        with self.assertRaises(ContainerNameExists):
+            db.add_container('thing 2')
+        with self.assertRaises(ContainerDoesNotExist):
+            db.add_container('thing 999', parent_id=999)
+        with self.assertRaises(ContainerDoesNotExist):
+            db.add_container('thing 5', parent_id=1, parent_name='thing 2')
+        with db._connection as conn:
+            result = conn.execute('SELECT container_id, name, parent_id FROM containers')
+        self.assertEqual([dict(r) for r in result.fetchall()],
+                         [{'container_id': 1, 'name': 'thing 1', 'parent_id': None},
+                          {'container_id': 2, 'name': 'thing 2', 'parent_id': None},
+                          {'container_id': 3, 'name': 'thing 3', 'parent_id': 1},
+                          {'container_id': 4, 'name': 'thing 4', 'parent_id': 1}])
+        
+    def test_get_container(self):
+        db = Database(':memory:')
+        with db._connection as conn:
+            conn.execute('INSERT INTO containers VALUES(1,"thing 1",NULL,"now","now")')
+            conn.execute('INSERT INTO containers VALUES(2,"thing 2",NULL,"now","now")')
+            conn.execute('INSERT INTO containers VALUES(3,"thing 3",1,"now","now")')
+        thing1 = {'container_id': 1, 'name': 'thing 1', 'parent_id': None,
+                  'created': 'now', 'modified': 'now'}
+        thing2 = {'container_id': 2, 'name': 'thing 2', 'parent_id': None,
+                  'created': 'now', 'modified': 'now'}
+        thing3 = {'container_id': 3, 'name': 'thing 3', 'parent_id': 1,
+                  'created': 'now', 'modified': 'now'}
+        self.assertEqual(db.get_container(id=1), thing1)
+        self.assertEqual(db.get_container(name='thing 1'), thing1)
+        self.assertEqual(db.get_container(id=1, name='thing 1'), thing1)
+        self.assertEqual(db.get_container(id=2), thing2)
+        self.assertEqual(db.get_container(name='thing 2'), thing2)
+        self.assertEqual(db.get_container(id=2, name='thing 2'), thing2)
+        self.assertEqual(db.get_container(id=3), thing3)
+        self.assertEqual(db.get_container(name='thing 3'), thing3)
+        self.assertEqual(db.get_container(id=3, name='thing 3'), thing3)
+        with self.assertRaises(ContainerDoesNotExist):
+            db.get_container(id=99)
+        with self.assertRaises(ContainerDoesNotExist):
+            db.get_container(name='not a container')
+        with self.assertRaises(ContainerDoesNotExist):
+            db.get_container(id=1, name='thing 2')
+        
+    def test_get_containers(self):
+        db = Database(':memory:')
+        self.assertEqual(db.get_containers(), [])
+        with db._connection as conn:
+            conn.execute('INSERT INTO containers VALUES(1,"thing 1",NULL,"now","now")')
+            conn.execute('INSERT INTO containers VALUES(2,"thing 2",NULL,"now","now")')
+            conn.execute('INSERT INTO containers VALUES(3,"thing 3",1,"now","now")')
+        thing1 = {'container_id': 1, 'name': 'thing 1', 'parent_id': None,
+                  'created': 'now', 'modified': 'now'}
+        thing2 = {'container_id': 2, 'name': 'thing 2', 'parent_id': None,
+                  'created': 'now', 'modified': 'now'}
+        thing3 = {'container_id': 3, 'name': 'thing 3', 'parent_id': 1,
+                  'created': 'now', 'modified': 'now'}
+        self.assertEqual(db.get_containers(), [thing1, thing2, thing3])
+
     def test_get_tables(self):
         db = Database(':memory:')
         self.assertEqual(db.get_tables(), ['versions', 'containers', 'notes'])
@@ -19,3 +83,48 @@ class TestDatabase(unittest.TestCase):
     def test_get_version_none(self):
         db = Database(':memory:', initialize=False)
         self.assertIsNone(db.get_version())
+
+
+class TestContainerDoesNotExist(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(ContainerDoesNotExist) as cm:
+            raise ContainerDoesNotExist()
+        self.assertEqual(str(cm.exception), 'Container does not exist.')
+        self.assertEqual(cm.exception.message, 'Container does not exist.')
+        self.assertEqual(cm.exception.id, None)
+        self.assertEqual(cm.exception.name, None)
+
+    def test_id(self):
+        with self.assertRaises(ContainerDoesNotExist) as cm:
+            raise ContainerDoesNotExist(id=1)
+        self.assertEqual(str(cm.exception), 'Container with (id=1) does not exist.')
+        self.assertEqual(cm.exception.message, 'Container with (id=1) does not exist.')
+        self.assertEqual(cm.exception.id, 1)
+        self.assertEqual(cm.exception.name, None)
+
+    def test_name(self):
+        with self.assertRaises(ContainerDoesNotExist) as cm:
+            raise ContainerDoesNotExist(name='bad')
+        self.assertEqual(str(cm.exception), 'Container with (name="bad") does not exist.')
+        self.assertEqual(cm.exception.message, 'Container with (name="bad") does not exist.')
+        self.assertEqual(cm.exception.id, None)
+        self.assertEqual(cm.exception.name, 'bad')
+
+    def test__name_and_id(self):
+        with self.assertRaises(ContainerDoesNotExist) as cm:
+            raise ContainerDoesNotExist(id=1, name='bad')
+        self.assertEqual(str(cm.exception), 'Container with (id=1,name="bad") does not exist.')
+        self.assertEqual(cm.exception.message, 'Container with (id=1,name="bad") does not exist.')
+        self.assertEqual(cm.exception.id, 1)
+        self.assertEqual(cm.exception.name, 'bad')
+
+
+class TestContainerNameExists(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(TypeError) as cm:
+            raise ContainerNameExists()
+        
+    def test_name(self):
+        with self.assertRaises(ContainerNameExists) as cm:
+            raise ContainerNameExists('bad')
+        self.assertEqual(str(cm.exception), 'Cannot add container named "bad", it already exists.')
